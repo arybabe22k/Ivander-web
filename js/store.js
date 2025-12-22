ee011243f001d830da2d6408a00bc4591392a0fa
arybabe22k <aristidesguilherme77@gmail.com>
2025-12-22 23:11:45 +0200

    Update product images and fix formatting

--- js/store.js
+++ js/store.js
@@ -1,24 +1,24 @@
 const products = [
-    // iPhone X
-    { brand: 'iphone', name: 'iPhone X', price: 28000, image: 'Imagens/produtos/ix.png' },
-    { brand: 'iphone', name: 'iPhone Xs', price: 30000, image: 'Imagens/produtos/ixs.png' },
-    { brand: 'iphone', name: 'iPhone Xs Max', price: 33000, image: 'Imagens/produtos/ixsmax.png' },
-    
-    // iPhone 11
-    { brand: 'iphone', name: 'iPhone XR', price: 29000, image: 'Imagens/produtos/xr.png' },
-    { brand: 'iphone', name: 'iPhone 11', price: 30000, image: 'Imagens/produtos/i11.png' },
-    { brand: 'iphone', name: 'iPhone 11 Pro', price: 35000, image: 'Imagens/produtos/i11pro.png' },
-    { brand: 'iphone', name: 'iPhone 11 Pro Max', price: 40000, image: 'Imagens/produtos/i11promax.png' },
+     // iPhone X
+    { brand: 'iphone', name: 'iPhone X', price: 28000, image: 'Imagens/produtos/iPhone/iP-X.jpg' },
+    { brand: 'iphone', name: 'iPhone Xs', price: 30000, image: 'Imagens/produtos/iPhone/iP-Xs.jpg' },
+    { brand: 'iphone', name: 'iPhone Xs Max', price: 33000, image: 'Imagens/produtos/iPhone/iP-Xs Max.png' },
+   
+  // iPhone 11
+    { brand: 'iphone', name: 'iPhone XR', price: 29000, image: 'Imagens/produtos/iPhone/iP-XR.jpg' },
+    { brand: 'iphone', name: 'iPhone 11', price: 30000, image: 'Imagens/produtos/iPhone/i11.png' },
+    { brand: 'iphone', name: 'iPhone 11 Pro', price: 35000, image: 'Imagens/produtos/iPhone/iP-11 Pro.jpg' },
+    { brand: 'iphone', name: 'iPhone 11 Pro Max', price: 40000, image: 'Imagens/produtos/iPhone/iP-11 Pro Max.jpg' },
 
     // iPhone 12
-    { brand: 'iphone', name: 'iPhone 12', price: 35000, image: 'Imagens/produtos/i12.png' },
-    { brand: 'iphone', name: 'iPhone 12 Pro', price: 40000, image: 'Imagens/produtos/i12pro.png' },
-    { brand: 'iphone', name: 'iPhone 12 Pro Max', price: 45000, image: 'Imagens/produtos/i12promax.png' },
+    { brand: 'iphone', name: 'iPhone 12', price: 35000, image: 'Imagens/produtos/iPhone/iP-12.jpg' },
+    { brand: 'iphone', name: 'iPhone 12 Pro', price: 40000, image: 'Imagens/produtos/iPhone/iP-12 Pro.jpg' },
+    { brand: 'iphone', name: 'iPhone 12 Pro Max', price: 45000, image: 'Imagens/produtos/iPhone/iP-12 Pro Max.jpg' },
 
     // iPhone 13
-    { brand: 'iphone', name: 'iPhone 13', price: 40000, image: 'Imagens/produtos/i13.png' },
-    { brand: 'iphone', name: 'iPhone 13 Pro', price: 45000, image: 'Imagens/produtos/i13pro.png' },
-    { brand: 'iphone', name: 'iPhone 13 Pro Max', price: 50000, image: 'Imagens/produtos/i13promax.png' },
+    { brand: 'iphone', name: 'iPhone 13', price: 40000, image: 'Imagens/produtos/iPhone/iP-13.jpg' },
+    { brand: 'iphone', name: 'iPhone 13 Pro', price: 45000, image: 'Imagens/produtos/iPhone/iP-13 Pro.jpg' },
+    { brand: 'iphone', name: 'iPhone 13 Pro Max', price: 50000, image: 'Imagens/produtos/iPhone/iP-13 Pro Max.jpg' },
 
     // iPhone 14
     { brand: 'iphone', name: 'iPhone 14', price: 45000, image: 'Imagens/produtos/i14.png' },
@@ -82,4 +82,5 @@ renderStore();
 function filterBrand(brand) {
     renderStore(brand);
 }
+
 
